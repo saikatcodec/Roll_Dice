b10d782bd7a0e86d813cba38f5526a1288957bdd
Joy Kumar Acharjee <joy29940@gmail.com>
2024-01-28 19:40:55 +0600

    add button and images in app

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-import 'package:first_app/styled_text.dart';
-
 class GradientContainer extends StatelessWidget {
   final List<Color> colors;
   const GradientContainer({super.key, required this.colors});
+
+  void rollDice() {}
 
   @override
   Widget build(context) {
@@ -16,8 +16,25 @@ class GradientContainer extends StatelessWidget {
           end: Alignment.bottomRight,
         ),
       ),
-      child: const Center(
-        child: StyledText('Hello World!'),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/dice-2.png',
+              width: 200,
+            ),
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(fontSize: 28),
+              ),
+              child: const Text('Roll Dice'),
+            ),
+          ],
+        ),
       ),
     );
   }
