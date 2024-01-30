88fb744109a4d28d3aa81842df12830fe8153a4d
Joy Kumar Acharjee <joy29940@gmail.com>
2024-01-30 14:52:04 +0600

    complete the roll dice app

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'package:first_app/dice_roller.dart';
+
 class GradientContainer extends StatelessWidget {
   final List<Color> colors;
   const GradientContainer({super.key, required this.colors});
-
-  void rollDice() {}
 
   @override
   Widget build(context) {
@@ -16,25 +16,8 @@ class GradientContainer extends StatelessWidget {
           end: Alignment.bottomRight,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/dice-2.png',
-              width: 200,
-            ),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(fontSize: 28),
-              ),
-              child: const Text('Roll Dice'),
-            ),
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
